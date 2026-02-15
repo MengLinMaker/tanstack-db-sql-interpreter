82e849baa738e7f06e7f5cea8fcbe1833439d0e7
Meng Lin <menglinmaker@gmail.com>
2026-02-15 22:19:47 +1100

    feat: export generated ast node types

--- packages/sql-parser/src/index.ts
+++ packages/sql-parser/src/index.ts
@@ -1,4 +1,5 @@
 import { parser } from '../dist/parser.js'
+import type * as Node from '../dist/treeType'
 import { transformTree } from './parser/transformTree.js'
 
 export const strictParseSql = (sql: string) => {
@@ -8,3 +9,7 @@ export const strictParseSql = (sql: string) => {
   const ast = transformTree(tree, sql)
   return ast
 }
+
+export type { Node }
+export type ChildrenName<T extends { children: { name: string }[] }> =
+  T['children'][number]['name']
