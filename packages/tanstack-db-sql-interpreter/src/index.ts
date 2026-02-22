14d1581a0c289b3f7a8ca8670e7db5468baa2b72
Meng Lin <menglinmaker@gmail.com>
2026-02-22 20:04:32 +1100

    feat: add callback to access query IR

--- packages/tanstack-db-sql-interpreter/src/index.ts
+++ packages/tanstack-db-sql-interpreter/src/index.ts
@@ -1,22 +1,40 @@
 import { strictParseSql } from '@menglinmaker/sql-parser'
-import type { InitialQueryBuilder } from '@tanstack/db'
+import type {
+  BaseQueryBuilder,
+  Context,
+  InitialQueryBuilder,
+  QueryBuilder,
+} from '@tanstack/db'
 import { cteNode } from './ast/cte.ts'
 import { selectStatementNode } from './ast/selectStatementNode.ts'
 import { defaultSwitchNodeError } from './util/error.ts'
 import type { Collections } from './util/types.ts'
 
-export const liveQuerySql = (collections: Collections, sql: string) => {
+type CallbackQueryIR = (
+  queryIr: ReturnType<BaseQueryBuilder['_getQuery']>,
+) => void
+
+export const liveQuerySql = (
+  collections: Collections,
+  sql: string,
+  callback: undefined | CallbackQueryIR = undefined,
+) => {
   const ast = strictParseSql(sql)
   const queryBuilder = (query: InitialQueryBuilder) => {
     const n = ast.children[0]
+    let q: QueryBuilder<Context>
     switch (n.name) {
       case 'SELECT_STATEMENT':
-        return selectStatementNode(n, query, collections)
+        q = selectStatementNode(n, query, collections)
+        break
       case 'CTE':
-        return cteNode(n, query, collections)
+        q = cteNode(n, query, collections)
+        break
       default:
         throw defaultSwitchNodeError(n)
     }
+    if (callback) callback((q as BaseQueryBuilder)._getQuery())
+    return q
   }
   return queryBuilder
 }
