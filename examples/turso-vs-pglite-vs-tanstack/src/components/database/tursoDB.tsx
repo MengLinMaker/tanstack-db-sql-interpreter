c71241dfedcf3e3cdb6ca40e009fd6ba3b88b991
Meng Lin <menglinmaker@gmail.com>
2026-02-26 22:13:37 +1100

    experimental view not yet supported

--- examples/turso-vs-pglite-vs-tanstack/src/components/database/tursoDB.tsx
+++ examples/turso-vs-pglite-vs-tanstack/src/components/database/tursoDB.tsx
@@ -1,6 +1,9 @@
 import { connect } from '@tursodatabase/database-wasm/vite'
 import { createContext } from 'solid-js'
 
-const db = await connect('local.db', { timeout: 1000 })
+const db = await connect('local.db', {
+  timeout: 1000,
+  // experimental: ['views'],
+})
 
 export const TursoDB = createContext(db)
