30d637511906814413f2bd01f446501572de2b8b
Meng Lin <menglinmaker@gmail.com>
2026-02-27 17:44:32 +1100

    don't clash opfs

--- examples/benchmark/src/components/database/tursoDB.tsx
+++ examples/benchmark/src/components/database/tursoDB.tsx
@@ -1,7 +1,7 @@
 import { connect } from '@tursodatabase/database-wasm/vite'
 import { createContext } from 'solid-js'
 
-const db = await connect('local.benchmark.db', {
+const db = await connect(`turso.benchmark.${new Date().toISOString()}.db`, {
   timeout: 1000,
   // experimental: ['views'],
 })
