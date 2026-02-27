7d1dbeef9715554f97844261eb06678ebf406984
Meng Lin <menglinmaker@gmail.com>
2026-02-28 00:42:17 +1100

    preview build

--- examples/benchmark/vite.config.ts
+++ examples/benchmark/vite.config.ts
@@ -2,19 +2,22 @@ import sqlocal from 'sqlocal/vite'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
+const crossOriginIsolationHandler = (server: {
+  middlewares: {
+    use: (arg0: (_req: any, res: any, next: any) => void) => void
+  }
+}) => {
+  server.middlewares.use((_req, res, next) => {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+    next()
+  })
+}
+
 const crossOriginIsolation = () => ({
   name: 'configure-server',
-  configureServer(server: {
-    middlewares: {
-      use: (arg0: (_req: any, res: any, next: any) => void) => void
-    }
-  }) {
-    server.middlewares.use((_req, res, next) => {
-      res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-      res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
-      next()
-    })
-  },
+  configureServer: crossOriginIsolationHandler,
+  configurePreviewServer: crossOriginIsolationHandler,
 })
 
 export default defineConfig({
