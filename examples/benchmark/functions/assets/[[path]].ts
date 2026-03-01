572f7cf5cffda94fecf833c3899b28fec4101ed3
Meng Lin <menglinmaker@gmail.com>
2026-03-01 22:17:31 +1100

    simplify path function cloudflare worker

--- examples/benchmark/functions/assets/[[path]].ts
+++ examples/benchmark/functions/assets/[[path]].ts
@@ -5,28 +5,17 @@ export async function onRequest(context: {
   next: () => Promise<Response>
 }) {
   const { request, env, next } = context
-
-  if (request.method !== 'GET' && request.method !== 'HEAD') {
-    return next()
-  }
-
   const url = new URL(request.url)
-  if (!url.pathname.endsWith('.wasm')) {
-    return next()
-  }
+  if (request.method !== 'GET' && request.method !== 'HEAD') return next()
+  if (!url.pathname.endsWith('.wasm')) return next()
 
-  const key = url.pathname.replace(/^\/+/, '')
+  const key = url.pathname
   const object = await env.WASM_BUCKET.get(key)
-  if (!object) {
-    return next()
-  }
+  if (!object) return next()
 
   const headers = new Headers()
   object.writeHttpMetadata(headers)
   headers.set('etag', object.httpEtag)
-  if (key.endsWith('.wasm')) {
-    headers.set('Content-Type', 'application/wasm')
-  }
   headers.set('Cache-Control', 'public, max-age=31536000, immutable')
 
   return new Response(object.body, { headers })
