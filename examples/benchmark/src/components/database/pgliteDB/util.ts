525390869e3c51489fac6884bb7cf1e9b5a8936b
MengLinMaker <menglinmaker@gmail.com>
2026-03-07 00:38:23 +1100

    use same db due to double wasm compile issue

--- examples/benchmark/src/components/database/pgliteDB/util.ts
+++ examples/benchmark/src/components/database/pgliteDB/util.ts
@@ -4,12 +4,13 @@ import { sqlSchema } from '../util/schema/schema.sql.ts'
 
 export type PgliteDb = PGliteWithLive
 
+const db = new PGlite('memory://', {
+  extensions: {
+    live,
+  },
+}) as never as PgliteDb
+
 export const pgliteFactory = async () => {
-  const db = new PGlite('memory://', {
-    extensions: {
-      live,
-    },
-  }) as never as PgliteDb
   await db.exec(sqlSchema)
   const tables = await db.query(
     "select tablename from pg_tables where schemaname = 'public' order by tablename;",
