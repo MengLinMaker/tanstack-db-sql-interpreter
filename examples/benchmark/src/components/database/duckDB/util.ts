53079914df7ccc72b13bbe9631b968b43e7f2f88
Meng Lin <menglinmaker@gmail.com>
2026-03-10 22:51:11 +1100

    feat: smaller duckdb wasm (#27)

--- examples/benchmark/src/components/database/duckDB/util.ts
+++ examples/benchmark/src/components/database/duckDB/util.ts
@@ -1,32 +1,17 @@
-import {
-  AsyncDuckDB,
-  ConsoleLogger,
-  type DuckDBBundles,
-  selectBundle,
-} from '@duckdb/duckdb-wasm'
+import { AsyncDuckDB, ConsoleLogger, selectBundle } from '@akabana/duckdb-wasm'
 // @ts-expect-error <no type>
-import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url'
+import eh_worker from '@akabana/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url'
 // @ts-expect-error <no type>
-import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url'
-// @ts-expect-error <no type>
-import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url'
-// @ts-expect-error <no type>
-import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url'
+import duckdb_wasm_eh from '@akabana/duckdb-wasm/dist/duckdb-eh.wasm?url'
 import { sqlSchema } from '../util/schema/schema.sql'
 
-const MANUAL_BUNDLES: DuckDBBundles = {
-  mvp: {
-    mainModule: duckdb_wasm,
-    mainWorker: mvp_worker,
-  },
-  eh: {
-    mainModule: duckdb_wasm_eh,
-    mainWorker: eh_worker,
-  },
-}
-
 export const duckdbFactory = async () => {
-  const bundle = await selectBundle(MANUAL_BUNDLES)
+  const bundle = await selectBundle({
+    eh: {
+      mainModule: duckdb_wasm_eh,
+      mainWorker: eh_worker,
+    },
+  } as never)
   const worker = new Worker(bundle.mainWorker!)
   const logger = new ConsoleLogger()
   const db = new AsyncDuckDB(logger, worker)
